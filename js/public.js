ccc516ec7eb0c50fcb91cb24921b6a4d0e6ab440
JackNice <1172248038@qq.com>
2017-11-27 18:16:35 +0800

    Signed-off-by: JackNice <1172248038@qq.com>

---
+++ js/public.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?cf3a5003c616ff09d7a24184e6824807";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
